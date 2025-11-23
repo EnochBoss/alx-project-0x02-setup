2d6f31247c35e781e193838f8fdb1499b95b30a8
EnochBoss <enoch.bosomtwe@gmail.com>
2025-11-23 22:53:26 +0000

    Add and use components for card and buttons

--- alx-project-0x02/components/common/PostCard.tsx
+++ alx-project-0x02/components/common/PostCard.tsx
@@ -3,6 +3,7 @@ import { PostProps } from "@/interfaces";
 export default function PostCard({ title, body, userId }: PostProps) {
   return (
     <div className="border p-4 mb-4 rounded bg-white">
+      <form />
       <h2 className="font-bold">{title}</h2>
       <p>{body}</p>
       <span className="text-gray-500 text-sm">User: {userId}</span>
